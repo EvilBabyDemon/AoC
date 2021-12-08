8997a9e9cec3ea8ad9c397dd5bf621332e16cfe9
EvilBabyDemon <lteufelbe@ethz.ch>
2021-12-08 15:19:37 +0100

    added code to time, made input reading lineshorter

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,26 @@
+use std::time::SystemTime;
 use std::io::BufRead;
 use std::io::BufReader;
 use std::io; 
 use std::fs::File; 
-mod day_9;
+mod day_8   ;
 
 fn main() {
-    
-    let filename = "input.txt";
+    let now = SystemTime::now();
+    let filename = "/home/lukas/Documents/VSC/AoC/AoC/target/debug/input.txt";
     println!("In file {}", filename);
 
     let input = file_to_vec(filename.to_string()).ok().unwrap();
-    day_9::__day9(input);
+    day_8::__day8(input);
     println!("Task 2:");
     let input2 = file_to_vec(filename.to_string()).ok().unwrap();
-    day_9::__day9_2(input2);
+    day_8::__day8_2(input2);
     
+    let time = now.elapsed().unwrap();
+    println!("Time: {}", time.as_micros());
 }
 
 
-fn file_to_vec(filename: String) -> io::Result<Vec<String>> { 
-    let file_in = File::open(filename)?; 
-    let file_reader = BufReader::new(file_in); 
-    Ok(file_reader.lines().filter_map(io::Result::ok).collect()) 
-} 
+fn file_to_vec(filename: String) -> io::Result<Vec<String>> {  
+    Ok(BufReader::new(File::open(filename)?).lines().filter_map(io::Result::ok).collect()) 
+}
