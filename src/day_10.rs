e7a7cee7022d1608cd57ac42094c8bb085cf4ee9
EvilBabyDemon <lteufebe@ethz.ch>
2021-12-10 08:45:53 +0100

    some stacks idk

---
+++ src/day_10.rs
@@ -0,0 +1,94 @@
+use std::collections::LinkedList;
+
+pub fn __day10(input: std::vec::Vec<std::string::String>) { 
+
+    let mut list: LinkedList<char> = LinkedList::new();
+    let mut count = 0;
+
+    for x in input {
+        let line : Vec<char> = x.chars().collect();
+        'inner: for y in line {
+            if y.eq(&'(') || y.eq(&'[') || y.eq(&'{') || y.eq(&'<') {
+                list.push_front(y);
+            } else {
+                let close = list.pop_front();
+
+                let hit = match close.unwrap() {
+                    '(' => if y.eq(&')') {true} else {false},
+                    '[' => if y.eq(&']') {true} else {false},
+                    '{' => if y.eq(&'}') {true} else {false},
+                    '<' => if y.eq(&'>') {true} else {false},
+                    _ => false,
+                };
+
+                if hit {
+                    continue;
+                } 
+
+               count += match y {
+                    ')' => 3,
+                    ']' => 57,
+                    '}' => 1197,
+                    '>' => 25137,
+                    _ => 0,
+                };
+                break 'inner;
+            }
+        }
+    }
+
+    println!("{}", count);
+
+} 
+
+pub fn __day10_2(input: std::vec::Vec<std::string::String>) { 
+
+    let mut counters = Vec::new();
+
+    'outer: for x in input {
+        let line : Vec<char> = x.chars().collect();
+        
+        let mut list: LinkedList<char> = LinkedList::new();
+        for y in line {
+            if y.eq(&'(') || y.eq(&'[') || y.eq(&'{') || y.eq(&'<') {
+                list.push_front(y);
+            } else {
+                let close = list.pop_front();
+                
+                let hit = match close.unwrap() {
+                    '(' => if y.eq(&')') {true} else {false},
+                    '[' => if y.eq(&']') {true} else {false},
+                    '{' => if y.eq(&'}') {true} else {false},
+                    '<' => if y.eq(&'>') {true} else {false},
+                    _ => false,
+                };
+
+                if hit {
+                    continue;
+                } 
+                continue 'outer;
+            }
+        }
+        let mut count : u64 = 0;
+        for y in list {
+            
+            count *= 5;
+            count += match y {
+                '(' => 1,
+                '[' => 2,
+                '{' => 3,
+                '<' => 4,
+                _ => 0,
+            };
+        }
+        counters.push(count);
+    }
+    counters.sort();
+
+    for x in &counters {
+        println!("{}", x);
+    }
+
+    println!("{}, with len {}", counters[counters.len()/2], counters.len());
+} 
+
