129e01e8d29198d6f479d4cd83d1213ee0a71240
EvilBabyDemon <lteufebe@ethz.ch>
2021-12-12 16:04:46 +0100

    cleaned again idk

--- src/day_12.rs
+++ src/day_12.rs
@@ -1,27 +1,23 @@
 use std::collections::HashMap;
 use std::collections::HashSet;
 
-
 pub fn __day12_2(input: std::vec::Vec<std::string::String>) { 
     
     let mut nodes : HashMap<&str, std::vec::Vec<&str>> = HashMap::new();
 
     for i in 0..input.len() {
         let line : Vec<&str> = input[i].split("-").collect();
-        let vect = nodes.entry(line[0]).or_insert(Vec::new());
-        vect.push(line[1]);
-        let vect = nodes.entry(line[1]).or_insert(Vec::new());
-        vect.push(line[0]);
+        nodes.entry(line[0]).or_insert(Vec::new()).push(line[1]);
+        nodes.entry(line[1]).or_insert(Vec::new()).push(line[0]);
     }
     let mut visited : std::collections::HashSet<&str> = HashSet::new();
     visited.insert("start");
     
     println!("Part 1: {}", paths(&nodes, &visited, "start", true));
     println!("Part 2: {}", paths(&nodes, &visited, "start", false));
-
 }
 
-fn paths (nodes: &HashMap<&str, std::vec::Vec<&str>>,  visited : &std::collections::HashSet<&str>, atm : &str, twice : bool) -> u16 {
+fn paths (nodes: &HashMap<&str, std::vec::Vec<&str>>,  visited : &std::collections::HashSet<&str>, atm : &str, twice : bool) -> u32 {
     if atm.eq("end") {
         return 1;
     }
